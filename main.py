be2d2b5954e7d905a52126b1c6dda56da97eb671
prateek041 <prateeksingh9741@gmail.com>
2022-01-29 12:04:10 +0530

    changed background screen color

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ SCREEN_HEIGHT = 800
 class AlienInvasion:
     # self initialisation of the class
     def __init__(self):
-        # for initialisation of all the pygame modules.
+        # initialising all the pygame modules.
         pg.init()
         # setting up display screen
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pg.display.set_caption("ALIEN INVASION")
 
+        # defining an attribute storing bg color of screen.
+        self.bg_color = (150, 150, 150)
+
     # main game loop.
     def run_game(self):
-        # the surface returned by display.set mode is refreshed again and again by this loop.
+        # the surface returned by display.set mode is refreshed repeatedly by this loop.
         while True:
             # this condition is used for clearing the event queue and "get" all the commands.
             # An event is an action that the user performs while playing the game. moving mouse etc.
-            """to access the events we use .event which returns a list of events performed since this function 
-            was last"""
+            # to access the events we use .event which returns a list of events performed since this function
+            # was last.
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
+            # setting the background color to the previously saved attribute. fill, fills a particular surface with
+            # selected color.
+            self.screen.fill(self.bg_color)
             # for refreshing the display screen i.e. to display the most recently drawn image.
             pg.display.flip()
 
