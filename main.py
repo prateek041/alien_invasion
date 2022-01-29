7f4907deafe48995636e52badbdc01f202c3bc3c
prateek041 <prateeksingh9741@gmail.com>
2022-01-29 09:04:15 +0530

    class definition

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+# importing pygame module
+import pygame as pg
+
+
+# this is the class managing all the basic functionalities of the program
+class AlienInvasion:
+    # self initialisation of the class
+    def __init__(self):
+        pg.init()
