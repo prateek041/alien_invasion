1f14c978aca5a12cc77c49381d414146fe70bfd0
prateek041 <prateeksingh9741@gmail.com>
2022-01-29 13:25:31 +0530

    completed ship.py, adding it further to main.py

--- ship.py
+++ ship.py
@@ -13,12 +13,13 @@ class Ship:
         self.screen = ai_game.screen
         # accessing the screen's rect attribute so that we can place our ship anywhere on the screen.
         self.screen_rect = ai_game.screen.get_rect()
-
+        # .load, loads the targeted image and returns a surface representing that image.
         self.image = pg.image.load('images/ship.bmp')
+        # accessing image's rect attribute so that we can use it later to place the ship.
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
-    def blitme(self):
-
+    def draw_me(self):
+        # blit, draws the source on the surface at the specified position.
         self.screen.blit(self.image, self.rect)
